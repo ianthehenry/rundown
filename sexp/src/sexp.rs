91099f091d353431c079c79f4b381b90be6bf513
Ian Henry <ianthehenry@gmail.com>
2021-05-06 14:51:02 -0700

    sexp: add a note about Cow<str>

--- sexp/src/sexp.rs
+++ sexp/src/sexp.rs
@@ -1,3 +1,9 @@
+// TODO: we could use a Cow<str> and save copying in the extremely
+// common case of a bare atoms or string literals that don't contain
+// any escape characters. But that would tie the lifetime of the Sexp
+// to the lifetime of the input. Which might be fine? I will have to
+// think about the tradeoffs there.
+
 #[derive(Debug)]
 pub enum Sexp {
     Atom(String),
